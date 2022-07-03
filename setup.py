2f634ae4d8e0d61c16c85456f1a8eeff1417b8c9
Joe Bylund <joseph.bylund@gmail.com>
2022-07-03 17:03:19 -0400

    (joe) cbor and boto

--- setup.py
+++ setup.py
@@ -8,10 +8,14 @@ setuptools.setup(
     author_email="joseph.bylund@gmail.com",
     author="Joe Bylund",
     description=description,
+    install_requires=[
+        "cbor2",  # TODO: do I want to just use pickle?
+        "boto3",
+    ],
     long_description=description,
     name="dynamodictionary",
     package_dir={"dynamodict": "src/dynamodict"},
     packages=["dynamodict"],
     url="https://github.com/jbylund/dynamodictionary",
-    version="1.0.2",
+    version="1.0.3",
 )
